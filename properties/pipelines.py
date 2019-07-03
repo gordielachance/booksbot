5fc3a57bf359cc156774ec53055d7a7c97e1bc5f
gordielachance <gordie.lachance+dev@gmail.com>
2019-07-03 12:18:20 +0200

    WIP

--- properties/pipelines.py
+++ properties/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class BooksPipeline(object):
+class PropertiesPipeline(object):
     def process_item(self, item, spider):
         return item
